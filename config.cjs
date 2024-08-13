a830923597d1a3128598f46ca4f5c978b5a4b61e
Taifur <147646965+Itxtaifur@users.noreply.github.com>
2024-08-13 05:08:52 +0500

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "",
+  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
   AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
@@ -18,11 +18,11 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "𓆩𝐋𝐔𝐂𝐈𝐅𝐄𝐑𓆪",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "4593707292",
-  GEMINI_KEY: process.env.GEMINI_KEY || "",
+  OWNER_NAME: process.env.OWNER_NAME || "🇵🇰⃢⃟👿𓆩𝐋𝐔𝐂𝐈𝐅𝐄𝐑𓆪 🇹🇷⃢⃟🖤",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "212612435972",
+  GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
 
 
-module.exports = config;
+module.exports = config;;
